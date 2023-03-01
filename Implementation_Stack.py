7665ccf448daec84cbe1b640581c5689ab89d370
Dylan Kamel <dylan.kamel@yahoo.ca>
2023-03-01 09:29:20 -0600

    Update Implementation_Stack.py

--- Implementation_Stack.py
+++ Implementation_Stack.py
@@ -1,48 +1,50 @@
-from self import self
-
 class Stack:
     def __init__(self, n):
         self.count = 0
         self.data = [None] * n
         self.max = n
+        self.top = 0
 
 
-    #pop inserts a data item at the end of the list
+
     def push(self, val):
         if self.count == self.max:
             print("Stack is full")
         else:
             self.data[self.count] = val
-            self.count = self.count + 1
+            self.count += 1
 
-    # removes and returns the last inserted data item
+
     def pop(self):
-        if self.isEmpty() == True:
-            return "Empty"
+        if self.count == 0:
+            print("Empty")
         else:
             temp = self.data[self.count - 1]
             self.data[self.count - 1] = None
             self.count -= 1
-            return temp
+            print(temp)
 
-    # returns the last inserted value
     def top(self):
         if self.isEmpty() == True:
-            return "Empty"
+            print("Empty")
         else:
-            return self.data[self.count - 1]
+            return self.data[self.top]
 
 
-    # returns the number of data items in the list
+
     def length(self):
-        return self.count
+        print(self.count)
 
-    # returns a true or false value if there is data in the list
+
     def isEmpty(self):
-        return self.count == 0
+        print(self.count == 0)
 
-# First round
-test = Stack(10)
+    def printStack(self):
+        index = 0
+        while index < self.count:
+            print(self.data[index])
+            index += 1
+            
 
 
 
